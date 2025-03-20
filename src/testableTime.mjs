58b0b0191001fda5eaf6d368cf7182dd59b51029
Yakovyakov <yyak0423@gmail.com>
2025-03-20 12:58:53 -0400

    exercise1: Refactored, testableTime to make it testable

--- src/testableTime.mjs
+++ src/testableTime.mjs
@@ -1,7 +1,12 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
+// The function depends directly on the current date (new Date()),
+// which makes testing difficult because the result changes depending on the day it's run.
+// To make it testable, we can modify the function to accept the current date as a parameter.
+// It can now be tested easily.
+
+export function daysUntilChristmas(now = new Date()) {
+
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
