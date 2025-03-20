1d94fe0a6d97fe7e30c90a32dea95a1f4d49420c
Yakovyakov <yyak0423@gmail.com>
2025-03-20 15:50:57 -0400

    exercise1: Make some tests for the original untestable1 using vi.fakeTimers

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,10 +1,43 @@
-import { describe, test } from "vitest";
+import { describe, test, beforeEach, afterEach, vi } from "vitest";
 import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
-describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+describe("Untestable 1: days until Christmas with vi.useFakeTimers", () => {
+  beforeEach(() => {
+    // Use fake timers
+    vi.useFakeTimers();
   });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  test("the Christmas day", () => {
+    const date = new Date(2025, 11, 25);
+    vi.setSystemTime(date);
+
+    expect(daysUntilChristmas()).toBe(0);
+  });
+
+  test("day before Christmas day", () => {
+    const date = new Date(2025, 11, 24);
+    vi.setSystemTime(date);
+
+    expect(daysUntilChristmas()).toBe(1);
+  });
+
+  test("day after Christmas day", () => {
+    const date = new Date(2025, 11, 26);
+    vi.setSystemTime(date);
+
+    expect(daysUntilChristmas()).toBe(364);
+  });
+
+  test("day after Christmas day (2024 is a leap year)", () => {
+    const date = new Date(2023, 11, 26);
+    vi.setSystemTime(date);
+
+    expect(daysUntilChristmas()).toBe(365);
+  });
+
 });
