cc8619bddc0546b1e29df61a4b16ceb2de0e0cf8
Yakovyakov <yyak0423@gmail.com>
2025-03-24 14:48:27 -0400

    exercise4: adding test for create and update using PostgresUserDao

--- test/testableGlobals.test.mjs
+++ test/testableGlobals.test.mjs
@@ -1,10 +1,18 @@
-import { afterEach, beforeEach, describe, test } from "vitest";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { InMemoryUserDao, PasswordService, PostgresUserDao } from "../src/testableGlobals.mjs";
 
+
 import argon2 from "@node-rs/argon2";
+import { exec } from 'child_process';
+
+import pg from "pg";
+import { promisify } from "util";
 
 
+const execAsync = promisify(exec);
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 describe("testableGlobals: enterprise application, test Password Service", () => {
   const userId = 1;
@@ -74,9 +82,9 @@ describe("InMemoryUserDao", () => {
     })
 
     const user1InDb = await dao.getById(users[0].userId);
-    const user2InBd = await dao.getById(users[1].userId);
+    const user2InDb = await dao.getById(users[1].userId);
     expect(user1InDb).to.deep.equal(users[0]);
-    expect(user2InBd).to.deep.equal(users[1]);
+    expect(user2InDb).to.deep.equal(users[1]);
     expect(await dao.getById(5)).to.equal(null);
   });
 
@@ -97,3 +105,109 @@ describe("InMemoryUserDao", () => {
 
 });
 
+async function isPostgresReady(host, port, user, password, database, maxAttempts = 10) {
+  const pool = new pg.Pool({
+    host,
+    port,
+    user,
+    password,
+    database,
+    max: 1,
+    idleTimeoutMillis: 5000,
+  });
+
+  for (let i = 0; i < maxAttempts; i++) {
+    try {
+      const client = await pool.connect();
+      await client.query('SELECT 1');
+      client.release();
+      return true;
+    } catch (err) {
+      if (i === maxAttempts - 1) throw err;
+      await sleep(2000);
+    }
+  }
+}
+
+async function connectTestDb() {
+  const service = "db";
+  const privatePort = "5432";
+
+  const resultPort = await execAsync(`docker compose port ${service} ${privatePort}`);
+  const [host, port] = resultPort.stdout.trim().split(":");
+  
+  const psResult =  await execAsync(`docker compose ps --quiet ${service}`);
+  const containerId = psResult.stdout.trim();
+
+  const inspectResult = await execAsync(`docker inspect ${containerId}`);
+  const envJson = JSON.parse(inspectResult.stdout)[0].Config.Env;
+  const env = envJson
+    .map((s) => s.split("="))
+    .reduce((m, [k, v]) => {
+      m[k] = v;
+      return m;
+    }, {});
+  await isPostgresReady(
+    host,
+    port,
+    env.POSTGRES_USER,
+    env.POSTGRES_PASSWORD,
+    env.POSTGRES_USER,
+  );
+
+  return new pg.Pool({
+    host,
+    port,
+    user: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    database: env.POSTGRES_USER,
+  });
+}
+
+describe("PostgresUserDao", () => {
+  let dao;
+  let db;
+  let userIdSeq = 100;
+
+  // We have to 
+  beforeAll(async () => {
+    
+    db = await connectTestDb();
+
+    // drop-tables.sql
+    await db.query("drop table if exists users;");
+
+    // create-tables.sql
+    await db.query("create table users(user_id integer primary key,password_hash varchar(100) not null);");
+
+    dao = new PostgresUserDao(db);
+  });
+
+
+  test("can get user by ID", async () => {
+    const users = [
+      {
+        userId: ++userIdSeq,
+        passwordHash: "passwordHashUser1",
+      },
+      {
+        userId: ++userIdSeq,
+        passwordHash: "passwordHashUser2",
+      },
+    ];
+    await Promise.all(users.map(async (user) => {
+      await dao.save(user);
+      const manualCheck = await dao.db.query('SELECT * FROM users WHERE user_id = $1', [user.userId]);
+    }));
+
+    const user1InDb = await dao.getById(users[0].userId);
+    const user2InDb = await dao.getById(users[1].userId);
+    expect(user1InDb).to.deep.equal(users[0]);
+    expect(user2InDb).to.deep.equal(users[1]);
+    expect(await dao.getById(5)).to.equal(null);
+  });
+
+  afterAll(async () => {
+    await db.end()
+  });
+});
